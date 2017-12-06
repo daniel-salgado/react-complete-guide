d9d7a477df123029d42a48c28526538b3d596b51
Daniel Salgado <daniel@salgadobr.com>
2017-12-06 17:58:17 +1300

    Dinamic style 2

--- src/App.js
+++ src/App.js
@@ -87,11 +87,26 @@ class App extends Component {
 
     }
 
+    //Dinamic CSS classes
+    //let classes = ['red', 'bold'].join(' ');
+    let classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+
+
+
+
     return (
 
       <div className="App">
         <h1>Hey</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           //onClick={() => this.switchNameHandler('Salgado!!')}>Switch Name</button>
